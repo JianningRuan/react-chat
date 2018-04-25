a79c35e9fa77bc91d320f5faae302860d13f595c
JianningRuan <675690521@qq.com>
2018-04-25 22:54:03 +0800

    基础

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { StaticRouter } from 'react-router-dom';
+import reducer from './store/reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+// const store = createStore(reducer, compose(applyMiddleware(thunk), window.devToolsExtension? window.devToolsExtension(): f=>f));
+
+ReactDOM.render(<Provider store={store}><StaticRouter><App /></StaticRouter></Provider>, document.getElementById('root'));
 registerServiceWorker();
